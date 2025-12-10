fd4232a735929f3547d4fa75a3f8c0af3d697123
Alfredo Ambruoso <ambruosoalfredo@gmail.com>
2025-12-10 19:54:40 +0100

    Add ScrollToTop component to fix navigation scroll behavior

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import AboutUs from './AboutUs';
 import Partners from './Partners';
+import ScrollToTop from './ScrollToTop';
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutUs />} />
